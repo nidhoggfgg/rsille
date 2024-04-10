2e9901529eb394f33208417772c71df7d4d2273e
nidhoggfgg <nidhoggfgg@gmail.com>
2024-04-10 17:55:21 +0800

    axis

--- src/extra/math/plot.rs
+++ src/extra/math/plot.rs
@@ -5,6 +5,7 @@ pub struct Plot<F> {
     step: f64,
     range: (f64, f64),
     scale: (f64, f64),
+    show_axis: bool,
 }
 
 impl<F> Plot<F>
@@ -17,13 +18,14 @@ where
     {
         let (start, end) = (range.0.into(), range.1.into());
         if start > end {
-            panic!("")
+            panic!("The start must greater than end")
         }
         Self {
             fny: f,
             range: (start, end),
             step: 0.01,
             scale: (10.0, 10.0),
+            show_axis: true
         }
     }
 }
@@ -40,10 +42,12 @@ where
     ) -> Result<(), crate::utils::RsilleErr>
     where
         T: Into<f64>,
-    {
+    {   
         let (x, y) = (x.into(), y.into());
         let (mut px, end) = self.range;
         let (sx, sy) = self.scale;
+        let (mut minx, mut maxx) = (px, end);
+        let (mut miny, mut maxy) = ((self.fny)(px), (self.fny)(px));
         loop {
             if px > end {
                 break;
@@ -51,6 +55,22 @@ where
             let py = (self.fny)(px);
             canvas.set(x + px * sx, y + py * sy);
             px += self.step;
+            if minx > px {
+                minx = px;
+            }
+            if maxx < px {
+                maxx = px;
+            }
+            if miny > py {
+                miny = py;
+            }
+            if maxy < py {
+                maxy = py;
+            }
+        }
+        if self.show_axis {
+            canvas.line((x + minx * sx, 0.0), (x + maxx * sx, 0.0));
+            canvas.line((0.0, y + miny * sy), (0.0, y + maxy * sy));
         }
         Ok(())
     }
