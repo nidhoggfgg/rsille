9f7023d4763d010c1d7cbecd28ab317d47640f31
nidhoggfgg <nidhoggfgg@gmail.com>
2025-04-17 18:18:54 +0800

    builder in render now support build render and eventloop

--- examples/basic_event_loop.rs
+++ examples/basic_event_loop.rs
@@ -1,12 +1,9 @@
-use rsille::{render::event_loop, tui::widgets::Text};
+use rsille::{render, tui::widgets::Text};
 
 fn main() {
     let num = "hello world!!!";
     let text = Text::new(&num.to_string());
 
-    let el = event_loop::Builder::new()
-        .full_screen()
-        .build(text)
-        .unwrap();
+    let el = render::Builder::new().full_screen().build_eventloop(text);
     el.run();
 }
